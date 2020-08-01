71ff376c7c09d20c32aed7cab2f17286f66ab030
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-08-01 18:30:24 -0400

    lessons 6-11

---
+++ playground/types.js
@@ -0,0 +1,80 @@
+/* eslint-disable */
+/*
+const name = 'wes';
+const middle = 'topher';
+const last = `bos`;
+
+const sentence = "she's so cool";
+const sentence2 = `she's so "cool"`;
+
+// console.log(sentence);
+
+const song = `Oooh
+
+yeah
+
+I
+
+like pizza`;
+
+// console.log(song);
+
+const hello = `hello my name is ${name}. Nice to meet you. I am ${1 +
+	100} years old.`;
+
+//console.log(hello);
+
+const html = `
+    <div>
+        <h2>${name}</h2>
+        <p>${hello}</p>
+    </div>
+`;
+
+document.body.innerHTML = html;
+*/
+
+/*
+const age = 100;
+const name = 'wes';
+
+const a = 10;
+const b = 20;
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+
+const what = smarties % kids;
+
+console.log(what);
+
+*/
+
+/*
+const person = {
+	first: 'wes',
+	last: 'bos',
+	age: 100,
+};
+
+console.log(person);
+*/
+
+/*
+let dog;
+dog = 'snickers';
+// console.log(dog);
+
+let somethingUndefined;
+const somethingNull = null;
+
+console.log(somethingUndefined, somethingNull);
+*/
+
+/*
+let isDrawing = false;
+const age = 18;
+const ofAge = age > 19;
+console.log(ofAge);
+*/
