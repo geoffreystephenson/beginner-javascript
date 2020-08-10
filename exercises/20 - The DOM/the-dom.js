3b520b85a1e5b18b9f50f217dce6e13ebc53a1fb
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-08-09 22:00:20 -0400

    lessons 20-22

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,22 @@
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
+
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+// // set the textContent property on that elment
+// // heading.textContent = 'Wes is cool';
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
+
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
+
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// pizzaList.insertAdjacentText('afterbegin', '🍕');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
