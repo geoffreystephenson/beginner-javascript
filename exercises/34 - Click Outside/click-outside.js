0262efa553f4bd302f04ab8cf24a828bddd964d8
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-09-01 18:15:14 -0400

    lesson 34

---
+++ exercises/34 - Click Outside/click-outside.js
@@ -0,0 +1,40 @@
+// console.log('it works!');
+
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
+function handleCardButtonClick() {
+	// console.log('YA CLICKED IT!');
+	const button = event.currentTarget;
+	const card = button.closest('.card');
+	// Grab the img src
+	const imgSrc = card.querySelector('img').src;
+	const desc = card.dataset.description;
+	const name = card.querySelector('h2').textContent;
+	// populate the modal with the new info
+	modalInner.innerHTML = `
+    <img width="600" height="600" src="${imgSrc.replace(
+		'200',
+		'600'
+	)}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+	// show the modal
+	modalOuter.classList.add('open');
+}
+
+function closeModal() {
+	modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(event) {
+	const isOutside = !event.target.closest('.modal-inner');
+	if (isOutside) {
+		closeModal();
+	}
+});
+
+cardButtons.forEach((button) =>
+	button.addEventListener('click', handleCardButtonClick)
+);
