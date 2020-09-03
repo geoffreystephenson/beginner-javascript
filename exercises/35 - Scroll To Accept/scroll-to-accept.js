d78c1af37381b2a571a53be3177239d8eb13ea52
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-09-03 16:20:01 -0400

    lesson 35

---
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,27 @@
+// console.log('it works!');
+
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+	if (payload[0].intersectionRatio === 1) {
+		button.disabled = false;
+		// stop observing the button
+		ob.unobserve(terms.lastElementChild);
+	}
+}
+
+const ob = new IntersectionObserver(obCallback, {
+	root: terms,
+	threshold: 1,
+});
+
+// ob.observe(watch);
+ob.observe(terms.lastElementChild);
+
+/*
+terms.addEventListener('scroll', (e) =>
+	console.log(e.currentTarget.scrollTop, e.currentTarget.scrollHeight)
+);
+*/
