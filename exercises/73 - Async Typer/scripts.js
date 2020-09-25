06c1f4ac0f3ac687a6831595857d10541e58e379
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-09-25 16:19:08 -0400

    lesson 73

---
+++ exercises/73 - Async Typer/scripts.js
@@ -0,0 +1,52 @@
+// console.log('it works!');
+
+function wait(ms = 0) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
+	return Math.floor(randomNumber * (max - min) + min);
+}
+
+// async for of loop
+// async function draw(el) {
+// 	// console.log(el);
+// 	const text = el.textContent;
+// 	let soFar = '';
+// 	for (const letter of text) {
+// 		// console.log(letter);
+// 		soFar += letter;
+// 		// console.log(soFar);
+// 		el.textContent = soFar;
+// 		// wait for some time
+// 		const { typeMin, typeMax } = el.dataset;
+// 		const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+// 		await wait(amountOfTimeToWait);
+// 	}
+// }
+
+// recursion
+function draw(el) {
+	// console.log(el);
+	let index = 1;
+	const text = el.textContent;
+	const { typeMin, typeMax } = el.dataset;
+	async function drawLetter() {
+		el.textContent = text.slice(0, index);
+		index += 1;
+		const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+		// exit condition
+		await wait(amountOfTimeToWait);
+		if (index <= text.length) {
+			drawLetter();
+			// wait for some time
+		}
+	}
+	// when this function draw() runs, kick off drawLetter
+	drawLetter();
+}
+
+// const els = document.querySelectorAll('[data-type]');
+// els.forEach((el) => draw(el));
+
+document.querySelectorAll('[data-type]').forEach(draw);
