ef9f1990d824c46befbd0e3df968cfde3154828b
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-10-01 20:27:27 -0400

    lesson 78

--- playground/modules/scripts.js
+++ playground/modules/scripts.js
@@ -1,5 +1,5 @@
 import first, { returnHi as sayHi, last, middle } from './utils.js';
-// import * as everything from './wes.js';
+import * as everything from './wes.js';
 import { handleButtonClick } from './handlers.js';
 
 const button = document.querySelector('button');
